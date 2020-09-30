3295b896fa289930712caf9e28d4e372e124f184
woojoo527 <woojoo1602@gmail.com>
2020-09-30 14:55:49 +0900

    Add Map Page

--- django_monitoring/django_monitoring/urls.py
+++ django_monitoring/django_monitoring/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings 
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('mainpage/', include('mainpage.urls')),
+    path('', lambda request: redirect('index', permanent=True)),
+    path('mainpage/', include('mainpage.urls'), name='mainpage'),
     path('admin/', admin.site.urls),
 ]
 
--- django_monitoring/mainpage/views.py
+++ django_monitoring/mainpage/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'template.html')
+    return render(request, 'mainpage.html')
+
+def mapview(request):
+    return render(request, 'map.html')
 
 # Create your views here.
