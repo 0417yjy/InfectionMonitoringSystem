f44ee78f93fad8707b60f41a9b3a11f76faf4107
Jongyeon Yoon <0417yjy@naver.com>
2020-11-08 00:34:35 +0900

    Delete duplicate static files

--- django_monitoring/django_monitoring/settings.py
+++ django_monitoring/django_monitoring/settings.py
@@ -119,8 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-STATIC_ROOT = os.path.join(os.path.join(BASE_DIR),"static-main")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
