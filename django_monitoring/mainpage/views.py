44ffd357815f1930d1fca7991fbe684d98a38d20
SeungMinSong2929 <lordroshan29@naver.com>
2020-11-17 13:30:17 +0900

    send API dict data to html

--- django_monitoring/mainpage/views.py
+++ django_monitoring/mainpage/views.py
@@ -8,3 +8,16 @@ def mapview(request):
     return render(request, 'map.html')
 
 # Create your views here.
+
+from . import keyword # 이거 고쳐야 함.
+
+# class FormView(generic.View): #formtest
+    # template_name = 'mainpage/contents-statistics.html'
+
+def statisticsView(self,request):
+    template_name = 'contents-statistics.html'
+    result = keyword.keywordFindAPI() #여기!
+    context =list({
+        'result' : result
+    })
+    return render(request, template_name, context)
