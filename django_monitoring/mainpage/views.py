ca1e1c27b263239969707baf764fe90f14ac579b
SeungMinSong2929 <lordroshan29@naver.com>
2020-11-19 18:47:29 +0900

    send API data to index.html

--- django_monitoring/mainpage/views.py
+++ django_monitoring/mainpage/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import keyword 
+
 def index(request):
+    #통계 받아오는 API로 가져옴
+    result = keyword.keywordFindAPI() 
+    #print(result)
+     #context는 html에 data로 넘겨주는 parameter들을 담는것. 각각의 값을 전달한다
+     #예를 들어 context에 result, result2, result3 이렇게 넣어서 전달하면
+     #index.html에서 result, result2, result3 변수를 html 태그나 javascript코드 등에서 사용 가능하다.
+    context ={
+        'result' : result
+    }
+    
     return render(request, 'index.html')
 
 def mapview(request):
@@ -9,15 +21,3 @@ def mapview(request):
 
 # Create your views here.
 
-from . import keyword # 이거 고쳐야 함.
-
-# class FormView(generic.View): #formtest
-    # template_name = 'mainpage/contents-statistics.html'
-
-def statisticsView(self,request):
-    template_name = 'contents-statistics.html'
-    result = keyword.keywordFindAPI() #여기!
-    context =list({
-        'result' : result
-    })
-    return render(request, template_name, context)
