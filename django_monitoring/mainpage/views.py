cc71178b640c8b46e1d9a26d3ba908d1b305860f
SeungMinSong2929 <lordroshan29@naver.com>
2020-11-19 18:59:56 +0900

    function index returns context

--- django_monitoring/mainpage/views.py
+++ django_monitoring/mainpage/views.py
@@ -6,7 +6,7 @@ from . import keyword
 def index(request):
     #통계 받아오는 API로 가져옴
     result = keyword.keywordFindAPI() 
-    #print(result)
+    print(result)
      #context는 html에 data로 넘겨주는 parameter들을 담는것. 각각의 값을 전달한다
      #예를 들어 context에 result, result2, result3 이렇게 넣어서 전달하면
      #index.html에서 result, result2, result3 변수를 html 태그나 javascript코드 등에서 사용 가능하다.
@@ -14,7 +14,7 @@ def index(request):
         'result' : result
     }
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 def mapview(request):
     return render(request, 'map.html')
