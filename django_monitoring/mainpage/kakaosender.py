6e6df85cb66ebec6ce6953c2d5bb55a71960edb2
Jongyeon Yoon <0417yjy@naver.com>
2020-12-03 02:57:15 +0900

    Update kakaotalk message template

--- django_monitoring/mainpage/kakaosender.py
+++ django_monitoring/mainpage/kakaosender.py
@@ -6,31 +6,32 @@ import json
 import requests
 #from models import Subscriber
 
-def send_to_kakao(address,lr, mr):
-    access_token = address # 인증키 받아옴
+
+def send_to_kakao(address, lr, mr, increased):
+    access_token = address  # 인증키 받아옴
     send_lists = []
     url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
 # 사용자 토큰
     headers = {
         "Authorization": "Bearer " + access_token
-        }
+    }
     if lr == "전체선택" and mr == "전체선택":
-        text = "안녕하세요, Team Lumos 입니다. 새 확진자가 발생했습니다."
+        text = "안녕하세요, Team Lumos 입니다. 새 확진자가 " + str(increased) + "명 발생했습니다."
     elif mr == "전체선택":
         my_region = lr
-        text = "안녕하세요, Team Lumos 입니다. "+my_region + "에서 확진자가 발생했습니다."
+        text = "안녕하세요, Team Lumos 입니다. "+my_region + "에서 새 확진자가 "  + str(increased) + "명 발생했습니다."
     else:
         my_region = lr + " " + mr
-        text = "안녕하세요, Team Lumos 입니다. "+my_region + "에서 확진자가 발생했습니다."
+        text = "안녕하세요, Team Lumos 입니다. "+my_region + "에서 확진자가 "  + str(increased) + "명 발생했습니다."
 
     data = {
-        "template_object" : json.dumps({ "object_type" : "text",
-                                     "text" : text,
-                                     "link" : {
-                                                 "web_url" : "http://127.0.0.1:8000/mainpage/"
-                                              }
-                                              })
-                                              }
+        "template_object": json.dumps({"object_type": "text",
+                                       "text": text,
+                                       "link": {
+                                           "web_url": "http://127.0.0.1:8000/mainpage/"
+                                       }
+                                       })
+    }
 
     response = requests.post(url, headers=headers, data=data)
     print(response.status_code)
@@ -38,4 +39,3 @@ def send_to_kakao(address,lr, mr):
         print('메시지를 성공적으로 보냈습니다.')
     else:
         print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
-
